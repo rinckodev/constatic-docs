7126ac372bc7b9be94b8be33ee3dca08aded0493
Rincko <guilhermericardopro71@gmail.com>
2024-11-28 09:42:50 -0400

    blog css fix

--- app/[lang]/blog/page.tsx
+++ app/[lang]/blog/page.tsx
@@ -4,9 +4,8 @@ import { FaNewspaper } from "react-icons/fa6";
 
 export default async function Page() {
 
-    return <div className="flex flex-col gap-2 px-12 py-4 w-full">
-        <div className="flex flex-col gap-2 p-8 border rounded-md
-        ">
+    return <div className="flex flex-col gap-2 p-2 lg:px-12 py-4 w-full">
+        <div className="flex flex-col gap-2 p-8 border rounded-md">
             <h1 className="flex items-center gap-4 text-4xl font-bold">
                <FaNewspaper/> Blog
             </h1>
