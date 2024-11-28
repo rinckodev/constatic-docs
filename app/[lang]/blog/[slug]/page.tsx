6b5b59e7681ea84fb42db1ed09e4080982ac18c8
Rincko <guilhermericardopro71@gmail.com>
2024-11-28 01:30:46 -0400

    fixed blog css

--- app/[lang]/blog/[slug]/page.tsx
+++ app/[lang]/blog/[slug]/page.tsx
@@ -17,7 +17,7 @@ export default async function Page(props: PageProps) {
     const page = blog.getPage([params.slug]);
     if (!page) notFound();
 
-    return <main className="flex flex-col gap-4 px-12 py-6">
+    return <main className="flex flex-col gap-4 px-2 lg:px-12 py-6">
         <div className="container rounded-xl px-8 py-4 border">
             <h1 className="mb-2 text-3xl font-bold darK:text-white">
                 {page.data.title}
@@ -30,18 +30,18 @@ export default async function Page(props: PageProps) {
             }
             <Link href="/blog" className={buttonVariants({ size: "sm", variant: "secondary" })}>Voltar</Link>
         </div>
-        <article className="container flex flex-col-reverse md:flex-row">
-            <div className="prose min-w-0 flex-1 px-4">
+        <article className="container flex flex-col-reverse md:flex-row gap-2">
+            <div className="prose min-w-0 flex-1">
                 <page.data.body components={defaultMdxComponents} />
             </div>
-            <div className="flex flex-col gap-4 md:border-l p-4 text-sm lg:w-[250px]">
+            <div className="flex flex-col gap-4 md:border-l md:p-4 text-sm lg:w-[250px]">
                 <div>
                     <p className="font-medium text-muted-foreground">
                         {new Date(page.data.date ?? page.file.name).toLocaleDateString()}
                     </p>
                 </div>
-                <InlineTOC items={page.data.toc} defaultOpen />
-            </div>
+                    <InlineTOC items={page.data.toc} defaultOpen/>
+                </div>
         </article>
     </main>
 }
