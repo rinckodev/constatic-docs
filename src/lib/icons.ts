a8ea2d27d41ba7417dce484e5198a04693fdc72b
rinckodev <guilhermericardopro71@gmail.com>
2025-02-22 11:06:46 -0400

    update docs

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -1,21 +1,23 @@
 import { IconType } from "react-icons";
-import * as ti from "react-icons/ti";
+import * as bi from "react-icons/bi";
 import * as bs from "react-icons/bs";
+import * as cg from "react-icons/cg";
 import * as fa from "react-icons/fa";
+import * as gr from "react-icons/gr";
 import * as hi from "react-icons/hi";
 import * as hi2 from "react-icons/hi2";
-import * as ri from "react-icons/ri";
 import * as io from "react-icons/io";
 import * as io5 from "react-icons/io5";
 import * as md from "react-icons/md";
-import * as tb from "react-icons/tb";
-import * as bi from "react-icons/bi";
-import * as si from "react-icons/si";
 import * as pi from "react-icons/pi";
-// import { DiscloudWhite } from "./public/svgs/icons/DiscloudWhite";
+import * as ri from "react-icons/ri";
+import * as si from "react-icons/si";
+import * as tb from "react-icons/tb";
+import * as ti from "react-icons/ti";
+import { DiscloudWhite } from "../../public/svgs/icons/DiscloudWhite";
 
 const LocalIcons = {
-    // DiscloudWhite
+    DiscloudWhite
 }
 const icons: Record<string, IconType> = {};
 const defaults = {
@@ -30,7 +32,7 @@ const defaults = {
 Object.assign(
     icons, defaults, ti, bs, 
     fa, hi, hi2, ri, io, io5, md, tb, bi, 
-    si, pi, 
+    si, pi, cg, gr,
     LocalIcons,
 );
 
